b52cf1fb68440974292a2ebb086b05a303b6c4f4
Kojo Bailey <50509420+KojoBailey@users.noreply.github.com>
2023-08-23 19:55:23 +0300

    Delete source directory
    Typo: source -> src

--- source/functions.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <iostream>
-#include <fstream>
-
-void sendError (std::string message);
-std::string getCellStr (std::istream& file, char delim);
-int getCellInt (std::istream& file, char delim);
--- source/json.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-std::string jsonBrace (std::string contents, bool end = 0);
-std::string jsonBox (std::string contents);
-std::string jsonLine (std::string name, std::string contents, bool end = 0);
-std::string jsonLine (std::string name, int contents, bool end = 0);
